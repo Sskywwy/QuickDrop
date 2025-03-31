82fcd168db174208e5cc4cc0148adcb07cf5d83b
Maxym Bogulskyi <sskywwy@gmail.com>
2025-03-31 21:51:51 +0300

    create component

---
+++ quickdropfront/src/app/components/file-upload/file-upload.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  imports: [],
+  templateUrl: './file-upload.component.html',
+  styleUrl: './file-upload.component.scss'
+})
+export class FileUploadComponent {
+  selectedFile: File | null = null;
+
+  onFileChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+    }
+  }
+
+  onSubmit(): void {
+    if (this.selectedFile) {
+      console.log('File selected:', this.selectedFile.name);
+      // Тут можна обробити завантаження файлу, наприклад, через сервіс.
+    } else {
+      console.log('No file selected');
+    }
+  }
+}
+
+
