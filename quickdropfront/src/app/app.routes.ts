43ddeb27ac10d6ca9d3e9441db94cbfe744d1f01
Maxym Bogulskyi <sskywwy@gmail.com>
2025-04-21 22:42:42 +0300

    -

--- quickdropfront/src/app/app.routes.ts
+++ quickdropfront/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+
 import path from 'node:path';
 
 export const routes: Routes = [];
--- quickdropfront/src/app/components/file-upload/file-upload.component.ts
+++ quickdropfront/src/app/components/file-upload/file-upload.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-file-upload',
+  standalone: true,
   imports: [],
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.scss'
